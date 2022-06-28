2c938583c8f37a761691afee2a2a00adfd45a325
Yuriy Pokhyl <woully@gmail.com>
2022-06-29 00:42:57 +0300

    finish

--- src/components/Box/Box.jsx
+++
@@ -1,9 +0,0 @@
-// import PropTypes from 'prop-types';
-
-// export const Box = ({ children }) => {
-//   return <div>{children}</div>;
-// };
-
-// Box.propTypes = {
-//   children: PropTypes.node,
-// };
